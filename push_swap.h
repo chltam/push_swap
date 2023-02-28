e1c7e3152b745628c9017fae61017fd5f49e560d
Hin Long Tam <htam@c4b9c3.42berlin.de>
2023-02-28 16:15:32 +0100

    done without bonus
    Committer: chltam <htam@c4b9c3.42berlin.de>

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: htam <htam@student.42berlin.de>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/07 16:38:20 by htam              #+#    #+#             */
-/*   Updated: 2023/02/23 22:34:18 by htam             ###   ########.fr       */
+/*   Updated: 2023/02/24 19:09:23 by htam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,6 @@ typedef struct s_info
 	size_t	size;
 }	t_info;
 
-
 t_stack		*init_stack(char **argv);
 void		ft_error(char *str);
 void		free_stack(t_stack **stack);
@@ -45,18 +44,17 @@ void		add_to_stack_bot(t_stack **stack, t_stack *new);
 size_t		count_node(t_stack *stack);
 void		push_action(t_info *info_a, t_info *info_b, char flag);
 void		rotate_action(t_info *info_a, t_info *info_b, char flag);
-void	re_rotate_action(t_info *info_a, t_info *info_b, char flag);
+void		re_rotate_action(t_info *info_a, t_info *info_b, char flag);
 void		swap_action(t_info *info_a, t_info *info_b, char flag);
-void	sort_three(t_info *info_a, t_info *info_b, char flag);
-void	sort_four(t_info *info_a, t_info *info_b);
-void	sort_five(t_info *info_a, t_info *info_b);
+void		sort_three(t_info *info_a, t_info *info_b, char flag);
+void		sort_four(t_info *info_a, t_info *info_b);
+void		sort_five(t_info *info_a, t_info *info_b);
 int			is_sorted(t_stack *stack);
-void	sort_more(t_info *info_a, t_info *info_b);
-void	radix_sort(t_info *info_a, t_info *info_b);
-int	find_max(t_stack *stack);
+void		sort_more(t_info *info_a, t_info *info_b, int limit);
+void		radix_sort(t_info *info_a, t_info *info_b);
+int			find_max(t_stack *stack);
 
-/*test*/
+/*print*/
 void		print_stack(t_stack *stack_a, t_stack *stack_b);
 
-/*test*/
 #endif
